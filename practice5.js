159f73d83befa2931117384cb355fca336f7dbf1
Daniel-Otieno <oderodanielotieno@gmail.com>
2025-05-26 07:06:52 +0300

    Js comments

---
+++ practice5.js
@@ -0,0 +1,6 @@
+// Declare variables
+let city = "Nairobi";     // Replace with your city
+let year = 2000;          // Replace with your birth year
+
+// Print the message
+console.log(`I was born in ${city} in ${year}.`);
